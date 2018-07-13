c5aac849911d4d00b59185282c74503ee459a7ee
Nick Brown <nickrossbrown@gmail.com>
2018-07-13 10:17:22 -0700

    add paramters for project and volunteer with default values set

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,9 @@
 class Project
+  attr_reader :id
+  attr_accessor :title
+  def initialize (parameters)
+    @title = parameters.fetch(:title, "UNDEFINED TITLE")
+    @id = parameters.fetch(:title, 0)
+
 
 end
--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,3 +1,17 @@
 class Volunteer
+  attr_reader :id, :project_id
+  attr_accessor :name
+  def initialize (args)
+    args = defaults.merge(args)
+    @name = :name
+    @id = :id
+    @project_id = :project_id
+   end
+
+   def defaults
+     {:name=>"NONE", :id=> 0, :project_id=>0}     
+   end
 
 end
+Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+expect(test_volunteer.name).to eq 'Jane'
